686f8497be7232746ec4a1ba279456b4d0836abf
chungwi <chungwi@whs.school.nz>
2023-02-14 15:11:11 +1300

    monkey

---
+++ Primitivevalues.java
@@ -0,0 +1,49 @@
+
+/**
+ * Write a description of class Primitivevalues here.
+ *
+ * @author (William chung)
+ * @version (14/2/23)
+ */
+public class Primitivevalues
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Primitivevalues
+     */
+    public Primitivevalues()
+    {
+        // initialise instance variables
+        x = 0;
+        System.out.println(x);
+        double EUL = 1254876587676547543447627643d;
+        System.out.println(EUL);
+        float floatthing = 24365654445f;
+        System.out.println(floatthing);
+    }
+    public int Booleanvariables;
+    {
+        boolean Booleanvariables = true;
+        Booleanvariables = false; 
+        System.out.println(Booleanvariables);
+    }
+    private int Lettersthing;
+    {
+        char letter ='a';
+        letter = 'b';
+        System.out.println(letter);
+    }
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
